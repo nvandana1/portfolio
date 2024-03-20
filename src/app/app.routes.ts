1391b282e32a9b657c759f16cf0dddef7a922248
nvand <nvandana1@gmail.com>
2024-03-20 18:12:44 +0530

    navigation set up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {ExperienceComponent} from "./components/experience/experience.component";
+import {ProjectComponent} from "./components/project/project.component";
+import {ContactComponent} from "./components/contact/contact.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [{path:'',redirectTo:'home',pathMatch:'full'},{path:'home',component:HomeComponent},{path:'experience',component:ExperienceComponent},{path:'project',component:ProjectComponent},{path:'contact',component:ContactComponent},];
