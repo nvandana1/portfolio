c3016d2446df6f24d5e3536f35d1f2e9f7989020
nvand <nvandana1@gmail.com>
2024-03-21 09:57:13 +0530

    home screen

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {FrameComponent} from "../frame/frame.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    FrameComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
